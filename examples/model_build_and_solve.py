6c010f48496d5026c6ed5412cbf5fe7a1f5dfa23
Harry Thawley <Harry.Thawley@anu.edu.au>
2025-11-04 13:26:47 +1100

    prototype

--- examples/model_build_and_solve.py
+++ examples/model_build_and_solve.py
@@ -17,6 +17,6 @@ model_build_time = time.time()
 print(model.scenarios)
 print(f"Model build time: {model_build_time - start_time:.4f} seconds")
 
-model.solve(scenarios="test")
+model.solve(scenarios="base")
 end_time = time.time()
 print(f"Model solve time: {end_time - model_build_time:.4f} seconds")
